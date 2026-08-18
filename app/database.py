c2ec104c349eabb4a65680de328c87a6095558e2
Aishwarya Toragalli <aishwaray7969@gmail.com>
2026-08-18 11:22:38 +0530

    Add automated quality checks

--- app/database.py
+++ app/database.py
@@ -1,27 +1,15 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base
-from sqlalchemy.orm import sessionmaker
-
 from app.config import settings
 
-engine = create_engine(
-    settings.database_url,
-    pool_pre_ping=True
-)
-
-SessionLocal = sessionmaker(
-    autocommit=False,
-    autoflush=False,
-    bind=engine
-)
-
-Base = declarative_base()
-
-
-def get_db():
-    db = SessionLocal()
-
-    try:
-        yield db
-    finally:
-        db.close()
+if settings.database_url.startswith("sqlite"):
+    engine = create_engine(
+        settings.database_url,
+        connect_args={
+            "check_same_thread": False
+        }
+    )
+else:
+    engine = create_engine(
+        settings.database_url,
+        pool_pre_ping=True
+    )
